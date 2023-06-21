5a9788004457a8f2115bc6547f3cc6fa77b9de89
С - 14 < - 14@DESKTOP-O8LQCCI>
2023-06-21 21:24:47 +0500

    Добавьте файлы проекта.

---
+++ CS_WF_1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CS_WF_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.richTextViewer = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(100)));
+            this.labelOutput.Location = new System.Drawing.Point(67, 94);
+            this.labelOutput.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(112, 25);
+            this.labelOutput.TabIndex = 0;
+            this.labelOutput.Text = "labelOutput";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInput.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxInput.Location = new System.Drawing.Point(72, 16);
+            this.textBoxInput.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(961, 38);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnOpen.Location = new System.Drawing.Point(762, 448);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(123, 51);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Открыть";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnOpen_MouseClick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.Location = new System.Drawing.Point(910, 448);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(123, 51);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // richTextViewer
+            // 
+            this.richTextViewer.AutoWordSelection = true;
+            this.richTextViewer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextViewer.Location = new System.Drawing.Point(72, 146);
+            this.richTextViewer.Name = "richTextViewer";
+            this.richTextViewer.Size = new System.Drawing.Size(961, 274);
+            this.richTextViewer.TabIndex = 3;
+            this.richTextViewer.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1058, 511);
+            this.Controls.Add(this.richTextViewer);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelOutput);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RichTextBox richTextViewer;
+    }
+}
+
---
+++ CS_WF_1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_WF_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            //labelOutput.Text = textBoxInput.Text;
+            labelOutput.ForeColor = Color.Aqua;
+            //или можно задать цвет:
+            //var color = new Color();
+            //color = Color.FromArgb(255, 0, 255, 0);
+            //labelOutput.ForeColor = color;
+            //labelOutput.Text = (Math.Pow(Convert.ToInt32(textBoxInput.Text), 2)).ToString();
+            
+            try
+            {
+                var number = Convert.ToInt32(textBoxInput.Text);
+                Math.Pow(number, 2);
+                var result = Math.Pow(number, 2).ToString();
+                labelOutput.Text = result;  
+            }
+            catch
+            {
+                var messange = "Не удалось преобразовать в число";
+                MessageBox.Show(messange);    
+                labelOutput.Text = messange;
+            }     
+        }
+
+        private void btnOpen_MouseClick(object sender, MouseEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            
+            openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog.Filter = "Текстовые (шаблон *.txt)|*.txt" +
+                "|Исходники (шаблон *.cs)|*.cs" +
+                "|Все (шаблон *.*)|*.*";
+            var result = openFileDialog.ShowDialog(this);
+            if(result == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(openFileDialog.FileName))
+                {
+                    richTextViewer.Text = sr.ReadToEnd();
+                }                    
+            }
+        }
+    }
+}
